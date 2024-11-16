82656277c51ad537cbde50f7d9a55be68b4a65b5
Diana Tuz <dianatuz12@gmail.com>
2024-11-17 01:09:09 +0200

    add section tech, responsive to about

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,117 +2,181 @@
 
 const translations = {
   en: {
-    navAbout: 'About',
-    navTech: 'Tech',
-    navBenefits: 'Benefits',
-    navContact: 'Contact',
+    aboutButtonNext: 'Next',
+    aboutButtonPlay: 'Play Video',
+    aboutButtonPrev: 'Previous',
+    aboutSubtitle:
+      'KAT loco is a foot-based VR locomotion system that gives complete physical control over lower-body actions, allowing you to freely walk, run, and carry out just any other movement in virtual reality.',
+    aboutTitle: 'ABOUT',
+    aboutTitleBlue: 'PRODUCT',
+    aboutTitleMob: 'ABOUT',
+    aboutTitleBlueMob: 'PRODUCT',
     buttonByu: 'Byu Now',
-    h1White: 'THE NEW START OF',
+    buttonNext: 'Next',
+    buttonPrev: 'Previous',
+    controlSubtitle:
+      'Our Multifunctional Software allows for quick access to KAT Loco’s control panel both from a computer desktop, and directly from your VR headset.',
+    controlTitle: 'VR/PC CONTROL PANEL',
+    educationSubtitle:
+      'Create aducational simulations, trainings and much more with unlimited virtual space and minimum physical space',
+    educationTitle: 'EDUCATION',
+    fitnesSubtitle:
+      'Combine business with pleasure, and discover countless ways to stay fit while playing your favorite VR Games!',
+    fitnesTitle: 'FITNESS',
     h1Blue: 'VR LOCOMOTION',
+    h1White: 'THE NEW START OF',
+    heroButtonPlay: 'Play Video',
     heroDescription:
       'Discover the most comprehensive VR Locomotion system, and unlock infinite motion in any games on any platforms!',
-    heroButtonPlay: 'Play Video',
-    aboutButtonPlay: 'Play Video',
-    meetButtonPlay: 'Play Video',
     heroFaq: 'FAQ',
     heroHelp: 'Help',
-    buttonPrev: 'Previous',
-    buttonNext: 'Next',
-    aboutButtonPrev: 'Previous',
-    aboutButtonNext: 'Next',
-    more: 'More',
     info2: 'MORE THAN',
     info2Blue: ' GAMING!',
     infoSubtitle: 'This also made for people who are interested in...',
-    educationTitle: 'EDUCATION',
-    educationSubtitle:
-      'Create aducational simulations, trainings and much more with unlimited virtual space and minimum physical space',
-    realEstateTitle: 'REAL ESTATE',
-    realEstateSubtitle:
-      'Desighn architectural projects in a deeply realistic environment allowing visitors to freely walk around, and feel their vibe',
-    fitnesTitle: 'FITNESS',
-    fitnesSubtitle:
-      'Combine business with pleasure, and discover countless ways to stay fit while playing your favorite VR Games!',
-    socialInteractionsTitle: 'SOCIAL INTERACTIONS',
-    socialInteractionsSubtitle:
-      'Hang out with your friends in the virtual world when you can’t meet space requirements',
-    whyh2: 'WHY',
-    whyh2Blue: 'KAT LOCO?',
-    universallyTitle: 'UNIVERSALLY COMPATIBLE',
-    universallySubtitle:
-      'KAT Loco offers universal compatibility across all major VR headsets and platforms allowing you to play any VR game with support for Free Locomotion',
-    controlTitle: 'VR/PC CONTROL PANEL',
-    controlSubtitle:
-      'Our Multifunctional Software allows for quick access to KAT Loco’s control panel both from a computer desktop, and directly from your VR headset.',
-    virelessTitle: 'WIRELESS SENSORS',
-    virelessSubtitle:
-      'What makes it even more advanced, KAT Loco is entirely wireless, and comes with a complete system of algorithms, super durable batteries and more!',
-    aboutTite: 'ABOUT',
-    aboutTiteBlue: 'PRODUCT',
-    aboutSubtitle:
-      'KAT loco is a foot-based VR locomotion system that gives complete physical control over lower-body actions, allowing you to freely walk, run, and carry out just any other movement in virtual reality.',
+    meetButtonPlay: 'Play Video',
     meetSubtitle:
       'KAT VR is an independent company dedicated to the research, development, and sales of VR Locomotion products and solutions. Founded in 2013, we have quickly grown to become one of the world’s leading professional suppliers of VR games’ & simulations’ equipment ',
     meetTitle: 'NICE TO MEET',
     meetTitleBlue: 'YOU!',
     meetTitleSmall: 'Hello,',
+    more: 'More',
+    navAbout: 'About',
+    navBenefits: 'Benefits',
+    navContact: 'Contact',
+    navTech: 'Tech',
+    realEstateSubtitle:
+      'Desighn architectural projects in a deeply realistic environment allowing visitors to freely walk around, and feel their vibe',
+    realEstateTitle: 'REAL ESTATE',
+    socialInteractionsSubtitle:
+      'Hang out with your friends in the virtual world when you can’t meet space requirements',
+    socialInteractionsTitle: 'SOCIAL INTERACTIONS',
+    universallySubtitle:
+      'KAT Loco offers universal compatibility across all major VR headsets and platforms allowing you to play any VR game with support for Free Locomotion',
+    universallyTitle: 'UNIVERSALLY COMPATIBLE',
+    virelessSubtitle:
+      'What makes it even more advanced, KAT Loco is entirely wireless, and comes with a complete system of algorithms, super durable batteries and more!',
+    virelessTitle: 'WIRELESS SENSORS',
+    whyh2: 'WHY',
+    whyh2Blue: 'KAT LOCO?',
+    connectionTitle: 'CONNECTION',
+    connectionWireless: 'Wireless: Bluetooth 4.2 Signal range: 5m ',
+    connectionReceiver: ' Receiver: USB 2.0 and above',
+    connectionTitle2: 'CONNECTION',
+    connectionWireless2: 'Wireless: Bluetooth 4.2 Signal range: 5m ',
+    connectionReceiver2: ' Receiver: USB 2.0 and above',
+    sensorTitle: 'SENSOR',
+    sensorWeight: 'Weight: 35g/1.23oz each ',
+    sensorDimension: 'Dimension: 50mm/1.97in 24mm/0.94in ',
+    sensorTitle2: 'SENSOR',
+    sensorWeight2: 'Weight: 35g/1.23oz each ',
+    sensorDimension2: 'Dimension: 50mm/1.97in 24mm/0.94in ',
+    sensorLight2: 'Light: LED lights',
+    sensorLight: 'Light: LED lights',
+    batteriesTitle: 'BATTERRIES',
+    batteriesType: 'Type: Lthium-lon polymer batteries',
+    batteriesCapacity: 'Capacity: 370mAh',
+    batteriesBatteryLife:
+      'Battery life: 10h of continuous use 150 hours on stand by ',
+    batteriesCharging:
+      'Charging: Fast charging - 1 hour Charging voltage and current: 5V = 0.5A',
+    batteriesTitle2: 'BATTERRIES',
+    batteriesType2: 'Type: Lthium-lon polymer batteries',
+    batteriesCapacity2: 'Capacity: 370mAh',
+    batteriesBatteryLife2:
+      'Battery life: 10h of continuous use 150 hours on stand by ',
+    batteriesCharging2:
+      'Charging: Fast charging - 1 hour Charging voltage and current: 5V = 0.5A',
+    techTitle: 'TECH',
+    techTitleBlue: 'SPECS',
   },
   ua: {
-    meetTitleSmall: 'Привіт,',
-    meetSubtitle:
-      'KAT VR — це незалежна компанія, яка займається дослідженнями, розробкою та продажем продуктів і рішень VR Locomotion. Заснована в 2013 році, ми швидко виросли і стали одним із провідних у світі професійних постачальників обладнання для VR-ігор і симуляторів.',
-    meetTitleBlue: 'ЗНАЙОМСТВУ!',
-    meetTitle: 'РАДИЙ ',
-    aboutTite: 'ПРО',
-    aboutTiteBlue: 'ПРОДУКТ',
+    techTitle: 'ТЕХНІЧНІ',
+    techTitleBlue: 'ХАРАКТЕРИСТИКИ',
+    aboutButtonNext: 'Наступний',
+    aboutButtonPlay: 'Відтворити',
+    aboutButtonPrev: 'Попередній',
     aboutSubtitle:
       'KAT loco — це ножна VR-система пересування, яка забезпечує повний фізичний контроль над діями нижньої частини тіла, дозволяючи вам вільно ходити, бігати та виконувати будь-які інші рухи у віртуальній реальності.',
-    virelessTitle: 'БЕЗДРОТОВІ ДАТЧИКИ',
-    virelessSubtitle:
-      'Що робить його ще більш досконалим, KAT Loco є повністю бездротовим і постачається з повною системою алгоритмів, надміцними батареями та іншим!',
-    controlTitle: 'ПАНЕЛЬ КЕРУВАННЯ VR/ПК',
+    aboutTitle: 'ПРО',
+    aboutTitleBlue: 'ПРОДУКТ',
+    aboutTitleMob: 'ПРО',
+    aboutTitleBlueMob: 'ПРОДУКТ',
+    buttonByu: 'Придбати',
+    buttonNext: 'Наступний',
+    buttonPrev: 'Попередній',
     controlSubtitle:
       'Наше багатофункціональне програмне забезпечення забезпечує швидкий доступ до панелі керування KAT Loco як із робочого столу комп’ютера, так і безпосередньо з гарнітури VR.',
-    navAbout: 'Про нас',
-    navTech: 'Характеристики',
-    navBenefits: 'Переваги',
-    navContact: 'Котакти',
-    buttonByu: 'Придбати',
-    h1White: 'НОВИЙ ПОЧАТОК',
+    controlTitle: 'ПАНЕЛЬ КЕРУВАННЯ VR/ПК',
+    educationSubtitle:
+      'Створюйте навчальні симуляції, тренінги та багато іншого з необмеженим віртуальним простором і мінімальним фізичним простором',
+    educationTitle: 'ОСВІТА',
+    fitnesSubtitle:
+      'Поєднайте приємне з бізнесом і відкрийте незліченну кількість способів залишатися у формі, граючи в улюблені VR-ігри!',
+    fitnesTitle: 'ФІТНЕС',
     h1Blue: 'VR LOCOMOTION',
+    h1White: 'НОВИЙ ПОЧАТОК',
+    heroButtonPlay: 'Відтворити',
     heroDescription:
       'Відкрийте для себе найповнішу систему VR Locomotion і розблокуйте нескінченний рух у будь-яких іграх на будь-якій платформі!',
-    heroButtonPlay: 'Відтворити',
-    aboutButtonPlay: 'Відтворити',
-    meetButtonPlay: 'Відтворити',
     heroFaq: 'FAQ',
     heroHelp: ' Допомога',
-    buttonPrev: 'Попередній',
-    buttonNext: 'Наступний',
-    aboutButtonPrev: 'Попередній',
-    aboutButtonNext: 'Наступний',
-    more: 'Більше',
     info2: 'БІЛЬШЕ, НІЖ',
     info2Blue: ' ІГРИ!',
     infoSubtitle: 'Це також призначено для людей, які цікавляться...',
-    educationTitle: 'ОСВІТА',
-    educationSubtitle:
-      'Створюйте навчальні симуляції, тренінги та багато іншого з необмеженим віртуальним простором і мінімальним фізичним простором',
-    realEstateTitle: 'НЕРУХОМІСТЬ',
+    meetButtonPlay: 'Відтворити',
+    meetSubtitle:
+      'KAT VR — це незалежна компанія, яка займається дослідженнями, розробкою та продажем продуктів і рішень VR Locomotion. Заснована в 2013 році, ми швидко виросли і стали одним із провідних у світі професійних постачальників обладнання для VR-ігор і симуляторів.',
+    meetTitle: 'РАДИЙ ',
+    meetTitleBlue: 'ЗНАЙОМСТВУ!',
+    meetTitleSmall: 'Привіт,',
+    more: 'Більше',
+    navAbout: 'Про нас',
+    navBenefits: 'Переваги',
+    navContact: 'Котакти',
+    navTech: 'Характеристики',
     realEstateSubtitle:
       'Розробляйте архітектурні проекти в глибоко реалістичному середовищі, дозволяючи відвідувачам вільно ходити та відчувати їхню атмосферу',
-    fitnesTitle: 'ФІТНЕС',
-    fitnesSubtitle:
-      'Поєднайте приємне з бізнесом і відкрийте незліченну кількість способів залишатися у формі, граючи в улюблені VR-ігри!',
-    socialInteractionsTitle: 'СОЦІАЛЬНІ ВЗАЄМОДІЇ',
+    realEstateTitle: 'НЕРУХОМІСТЬ',
     socialInteractionsSubtitle:
       'Спілкуйтеся з друзями у віртуальному світі, коли вам не вистачає місця',
-    whyh2: 'ЧОМУ',
-    whyh2Blue: 'KAT LOCO?',
-
-    universallyTitle: 'УНІВЕРСАЛЬНА СУМІСНІСТЬ',
+    socialInteractionsTitle: 'СОЦІАЛЬНІ ВЗАЄМОДІЇ',
     universallySubtitle:
       'KAT Loco пропонує універсальну сумісність з усіма основними гарнітурами та платформами VR, дозволяючи вам грати в будь-яку VR-ігру з підтримкою Free Locomotion',
+    universallyTitle: 'УНІВЕРСАЛЬНА СУМІСНІСТЬ',
+    virelessSubtitle:
+      'Що робить його ще більш досконалим, KAT Loco є повністю бездротовим і постачається з повною системою алгоритмів, надміцними батареями та іншим!',
+    virelessTitle: 'БЕЗДРОТОВІ ДАТЧИКИ',
+    whyh2: 'ЧОМУ',
+    whyh2Blue: 'KAT LOCO?',
+    sensorTitle: 'ДАТЧИК',
+    connectionTitle: 'ПІДКЛЮЧЕННЯ',
+    connectionSubtitle:
+      'Бездротовий зв’язок: Bluetooth 4.2 Діапазон сигналу: 5 м Приймач: USB 2.0 і вище',
+    sensorWeight: 'Вага: 35 г/1,23 унції кожен',
+    sensorDimension: 'Розмір: 50 мм/1,97 дюйма 24 мм/0,94 дюйма ',
+    sensorLight: 'Світло: світлодіодні ліхтарі',
+    batteriesCharging:
+      'Заряджання: Швидке заряджання - 1 година Напруга та струм заряджання: 5 В = 0,5 А',
+    batteriesTitle: 'БАТАРЕЇ',
+    batteriesTitle2: 'БАТАРЕЇ',
+    batteriesType: 'Тип: Lthium-lon полімерні батареї ',
+    batteriesCapacity: 'Ємність: 370 мАг ',
+    batteriesBatteryLife:
+      'Термін служби батареї: 10 годин безперервного використання 150 годин у режимі очікування ',
+    sensorTitle2: 'ДАТЧИК',
+    connectionTitle2: 'ПІДКЛЮЧЕННЯ',
+    connectionSubtitle2:
+      'Бездротовий зв’язок: Bluetooth 4.2 Діапазон сигналу: 5 м Приймач: USB 2.0 і вище',
+    sensorWeight2: 'Вага: 35 г/1,23 унції кожен',
+    sensorDimension2: 'Розмір: 50 мм/1,97 дюйма 24 мм/0,94 дюйма ',
+    sensorLight2: 'Світло: світлодіодні ліхтарі',
+    batteriesCharging2:
+      'Заряджання: Швидке заряджання - 1 година Напруга та струм заряджання: 5 В = 0,5 А',
+    batteriesTitle2: 'БАТАРЕЇ',
+    batteriesType2: 'Тип: Lthium-lon полімерні батареї ',
+    batteriesCapacity2: 'Ємність: 370 мАг ',
+    batteriesBatteryLife2:
+      'Термін служби батареї: 10 годин безперервного використання 150 годин у режимі очікування ',
   },
 };
 
@@ -148,10 +212,18 @@ function showAboutNextImage() {
   document.getElementById('sliderAbout').style = 'transform: translate(0%)';
   document.getElementById('aboutButtonNext').classList.remove('buttonActive');
   document.getElementById('aboutButtonPrev').classList.add('buttonActive');
+  document.getElementById('aboutDotNext').classList.remove('dotActive');
+  document.getElementById('aboutDotPrev').classList.add('dotActive');
 }
 
 function showAboutPrevImage() {
   document.getElementById('sliderAbout').style = 'transform: translate(-100%)';
   document.getElementById('aboutButtonPrev').classList.remove('buttonActive');
   document.getElementById('aboutButtonNext').classList.add('buttonActive');
+  document.getElementById('aboutDotPrev').classList.remove('dotActive');
+  document.getElementById('aboutDotNext').classList.add('dotActive');
+}
+
+function displayInfo(id) {
+  document.getElementById(id).classList.toggle('displayedInfo');
 }
